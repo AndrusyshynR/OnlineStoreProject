7cba62886e48cdccc85f9a53caa50b19c9df8f40
AndrusyshynR <romanow3333@gmail.com>
2021-05-11 19:44:37 +0300

    new commit

--- src/js/backet.js
+++ src/js/backet.js
@@ -6,65 +6,69 @@ var arrBasket = {};
 	var goods = await response.json();
 
 	checkBasket();
-	displayArrBasket();
+	displayArrBasket(goods);
 
-	function displayArrBasket() {
-		for (var key in arrBasket) {
-			//Вычисляю сумму товара 
-			var sum = `${arrBasket[key]}` * `${goods[key].cost}`;
+	//Отрисовка товара в корзине
+	// function displayArrBasket() {
+	// 	for (var key in arrBasket) {
+	// 		//Вычисляю сумму товара 
+	// 		var sum = `${arrBasket[key]}` * `${goods[key].cost}`;
 
-			var basketProdukt = document.querySelector('.basket-store');
-			var produktDiv = document.createElement('div');
+	// 		var basketProdukt = document.querySelector('.basket-store');
+	// 		var produktDiv = document.createElement('div');  //!добавляю каждый раз новый див 
 
-			var newPostBasket = `<div class="basket-produkt">
-			<button class="all-btns delite-all" data-art="${key}">X</button>
-			<p class="name-goods-backet">Почти твоя ${goods[key].name}</p>
-			<img class="img-arrbasket" src ="${goods[key].img}">
-			<p>${arrBasket[key]}</p>
-			<span class="sum">${sum}грн</span>
-			<button class="all-btns minus-product" data-art="${key}">-</button>
-			<button class="all-btns add-product" data-art="${key}">+</button>
-		</div>`
+	// 		var newPostBasket = `<div class="basket-produkt">
+	// 		<button class="all-btns delite-all" data-art="${key}">X</button>
+	// 		<p class="name-goods-backet">Почти твоя ${goods[key].name}</p>
+	// 		<img class="img-arrbasket" src ="${goods[key].img}">
+	// 		<p>${arrBasket[key]}</p>
+	// 		<span class="sum">${sum}грн</span>
+	// 		<button class="all-btns minus-product" data-art="${key}">-</button>
+	// 		<button class="all-btns add-product" data-art="${key}">+</button>
+	// 		</div>`
 
-			produktDiv.innerHTML = newPostBasket;
-			basketProdukt.appendChild(produktDiv);
-		}
-	}
-	
-	//!корзина перерисововается только после перезагруски страницы
-	//!Удаление товара с корзины
+	// 		produktDiv.innerHTML = newPostBasket;
+	// 		basketProdukt.appendChild(produktDiv); //! ты должен перезатирать а не добавлять
+	// 	}
+	// }
+
+	//Удаление товара с корзины
 	var deliteGoods = document.querySelectorAll('.delite-all');
 	for (var i = 0; i < deliteGoods.length; i++) {
 		deliteGoods[i].addEventListener('click', delGoods);
-	}
+	};
 
 	function delGoods(event) {
 		var articul = event.target.dataset.art;
 		delete arrBasket[articul];
 
 		saveArrBasketLs();
-		displayArrBasket();
-	}
+		// displayArrBasket();
 
-	//!Добавление товара в корзине
+		location.reload();
+	};
+
+	//Добавление товара в корзине
 	var btnAdd = document.querySelectorAll('.add-product');
 	for (var i = 0; i < btnAdd.length; i++) {
 		btnAdd[i].addEventListener('click', addGoods);
-	}
+	};
 
 	function addGoods(event) {
 		var articul = event.target.dataset.art;
 		arrBasket[articul]++;
 
 		saveArrBasketLs();
-		displayArrBasket();
-	}
+		// displayArrBasket();
 
-	//!Отбавление количества товара в корзине
+		location.reload();
+	};
+
+	//Отбавление количества товара в корзине
 	var btnMinus = document.querySelectorAll('.minus-product');
 	for (var i = 0; i < btnMinus.length; i++) {
 		btnMinus[i].addEventListener('click', minusGoods);
-	}
+	};
 
 	function minusGoods(event) {
 		var articul = event.target.dataset.art;
@@ -74,12 +78,38 @@ var arrBasket = {};
 		} else {
 			delete arrBasket[articul];
 		}
+
 		saveArrBasketLs();
-		displayArrBasket();
-	}
+		// displayArrBasket();
+
+		location.reload();
+	};
 
 })();
 
+//Отрисовка товара в корзине
+function displayArrBasket(arrGoods) {
+	for (var key in arrBasket) {
+		//Вычисляю сумму товара 
+		var sum = `${arrBasket[key]}` * `${arrGoods[key].cost}`;
+
+		var basketProdukt = document.querySelector('.basket-store');
+		var produktDiv = document.createElement('div');
+
+		var newPostBasket = `<div class="basket-produkt">
+			<button class="all-btns delite-all" data-art="${key}">X</button>
+			<p class="name-goods-backet">Почти твоя ${arrGoods[key].name}</p>
+			<img class="img-arrbasket" src="${arrGoods[key].img}">
+			<p>${arrBasket[key]}</p>
+			<span class="sum">${sum}грн</span>
+			<button class="all-btns minus-product" data-art="${key}">-</button>
+			<button class="all-btns add-product" data-art="${key}">+</button>
+			</div>`
+
+		produktDiv.innerHTML = newPostBasket;
+		basketProdukt.appendChild(produktDiv); //! перезатирать а не добавлять
+	}
+};
 
 //Проверка товара в корзине
 function checkBasket() {
@@ -90,6 +120,10 @@ function checkBasket() {
 
 function saveArrBasketLs() {
 	localStorage.setItem('arrBasket', JSON.stringify(arrBasket));
-}
+};
 
 console.log('backet--->', 'backet');
+
+
+//todo 1. В корзине сделать общею сумму за весь товар.
+//todo 2. после кнопки оплата сделать попап
--- src/js/main.js
+++ src/js/main.js
@@ -1,106 +1,145 @@
-//Моя корзина
-var arrBasket = {};
+document.addEventListener('DOMContentLoaded', function () {
 
-checkBasket();
+	//Моя корзина
+	var arrBasket = {};
 
-//Создания карточки товара
-(async () => {
-	var response = await fetch('./src/js/goods.json');
-	var myProduct = await response.json();
+	checkBasket();
 
-	for (var key in myProduct) {
-		var importPost = document.querySelector('.produkt-card');
-		var createDiv = document.createElement('div');
+	//Создания карточки товара
+	(async () => {
+		const response = await fetch('./src/js/goods.json');
+		var myProduct = await response.json();
 
-		var newPost = `<div class="card"><img class="img-page" src ="${myProduct[key].img}"> <p class="name">${myProduct[key].name}</p> <div class="card-nav">
-		<button id="btns-js" class="btns" type="button" data-art="${key}">Добавить в корзину</button> <span class="cost">${myProduct[key].cost}грн</span></div></div>`
+		function displayGoods() {
+			for (var key in myProduct) {
+				var importPost = document.querySelector('.produkt-card');
+				var createDiv = document.createElement('div');
 
-		createDiv.innerHTML = newPost;
-		importPost.appendChild(createDiv);
-	}
+				var newPost = `<div class="card">
+						<img class="img-page" src ="${myProduct[key].img}">
+						<a class='link-name' href='/product.html' data-ar="${key}" target='_blank'>${myProduct[key].name}</a>
+					<div class="card-nav">
+						<button id="btns-js" class="btns" type="button" data-art="${key}">Добавить в корзину</button>
+						<span class="cost">${myProduct[key].cost}грн</span>
+					</div></div>`
 
-	// Добавления товара в корзину
-	var cardButtons = document.querySelectorAll('.btns');
+				createDiv.innerHTML = newPost;
+				importPost.appendChild(createDiv);
+			};
+		};
 
-	for (var i = 0; i < cardButtons.length; i++) {
-		cardButtons[i].addEventListener('click', addBasket);
-	}
+		displayGoods();
 
-	function addBasket(event) {
-		var articul = event.target.dataset.art;
+		//!Открытие товара в новом коне
+		var linkProduct = document.querySelectorAll('.link-name');
 
-		if (arrBasket[articul] != undefined) {
-			arrBasket[articul]++;
-		} else {
-			arrBasket[articul] = 1;
+		for (var i = 0; i < linkProduct.length; i++) {
+			linkProduct[i].addEventListener('click', openProductNewWindow);
 		}
 
-		localStorage.setItem('arrBasket', JSON.stringify(arrBasket));
+		function openProductNewWindow(e) {
+			var artikulProduct = e.target.dataset.ar;
+			console.log(artikulProduct);
 
-		// showBasket();
-	}
+			var productView = document.querySelector('.product-view');
+			console.log(productView);
 
-	//!сортировка
-	var changeSelect = document.querySelector('.search-sel');
-	changeSelect.addEventListener('change', mySort);
+			var productViewDiv = document.createElement('div');
 
-	function mySort(e) {
-		for (var items in myProduct) {
-			console.log(myProduct[items].name);
+			var newPro = `<div class="card">
+				<img class="img-page" src ="${myProduct[artikulProduct].img}">
+				<a class='link-name' href='./product.html'" target='_blank'>${myProduct[artikulProduct].name}</a>
+			<div class="card-nav">
+					<span class="cost">${myProduct[artikulProduct].cost}грн</span>
+			</div></div>`
 
-			if (e.target.value === 'AZ') {
-				myProduct[items].name.sort(function (a, b) {
-					if (a.name < b.name) {
-						return -1;
-					}
-					if (a.name > b.name) {
-						return 1;
-					}
-					return 0;
-				})
+			// productViewDiv.innerHTML = newPro;
+			// productView.appendChild(productViewDiv);
+		};
 
-			} else if (e.target.value === 'ZA') {
-				myProduct[items].name.sort(function (a, b) {
-					if (a.name > b.name) {
-						return -1;
-					}
-					if (a.name < b.name) {
-						return 1;
-					}
-					return 0;
-				})
+		// Добавления товара в корзину
+		var cardButtons = document.querySelectorAll('.btns');
+
+		for (var i = 0; i < cardButtons.length; i++) {
+			cardButtons[i].addEventListener('click', addBasket);
+		}
+
+		function addBasket(event) {
+			var articul = event.target.dataset.art;
+
+			console.log(articul);
+
+			if (arrBasket[articul] != undefined) {
+				arrBasket[articul]++;
+			} else {
+				arrBasket[articul] = 1;
 			}
+
+			localStorage.setItem('arrBasket', JSON.stringify(arrBasket));
+
+			// showBasket();
 		}
-	}
+
+		//!сортировка по цене
+		var changeSelect = document.querySelector('.search-sel');
+		changeSelect.addEventListener('change', mySort);
+
+		function mySort(e) {
+			if (e.target.value === 'AZ') {
+				var sortAz = myProduct.sort((a, b) => Number(b.cost) - Number(a.cost));
+				displayGoods(sortAz);
+			} else if (e.target.value === 'ZA') {
+				var sortZa = myProduct.sort((a, b) => Number(a.cost) - Number(b.cost));
+				displayGoods(sortZa);
+			};
+		};
+
+		//!Сортировка по названию
+		// var changeSelect = document.querySelector('.search-inp');
+		// changeSelect.addEventListener('input', sortName);
+
+		// function sortName() {
+		// 	var val = this.value.trim();
+
+		// 	if (val !== '') {
+		// 		for (var key in myProduct) {
+		// 			console.log(myProduct[key].name);
+		// 			if ( key.innerText.search(val) == -1 ) {
+		// 				displayGoods();
+		// 			}
+		// 		}
+		// 	}
+		// }
+
+	})();
+
+	//Проверка товара в корзине
+	function checkBasket() {
+		if (localStorage.getItem('arrBasket') != null) {
+			arrBasket = JSON.parse(localStorage.getItem('arrBasket'));
+		}
+	};
+
+	//!Отображения количества товара возле корзины покупок
+	// function showBasket() {
+	// 	for (var items in arrBasket) {
+	// 		var countGoods = arrBasket[items];
 
 
-})();
+	// 		var miniBasketResult = document.querySelector('.basket-res')
+	// 		var basketRes = document.createElement('div');
 
-//Проверка товара в корзине
-function checkBasket() {
-	if (localStorage.getItem('arrBasket') != null) {
-		arrBasket = JSON.parse(localStorage.getItem('arrBasket'));
-	}
-};
+	// 		var newPost = `<span>${countGoods}</span>`
 
-//!Отображения товара в корзине покупок
-//!Добавляет товар копируя предедущий на еденицу
-// var miniBasketResult = document.querySelectorAll('.basket-res'); //Доступ к спану корзины
-
-// function showBasket() {
-// 	for (var items in arrBasket) {
-// 		var countGoods =  arrBasket[items]++;
-
-// 		var miniBasketResult = document.querySelector('.basket-res')
-// 		var basketRes = document.createElement('div');
-
-// 		var newPost = `<span>${countGoods}</span>`
-
-// 		basketRes.innerHTML = newPost;
-// 		miniBasketResult.appendChild(basketRes);
-// 	}
-// };
-
-// showBasket();
+	// 		basketRes.innerHTML = newPost;
+	// 		// miniBasketResult.appendChild(basketRes);
+	// 		miniBasketResult.innerHTML = countGoods;
+	// 	}
+	// };
+	// showBasket();
+});
 
 console.log('main--->', 'main');
+
+//todo 1.Фильтры по цене и имени
+//todo 2.Переход на карточку товара отдельно
